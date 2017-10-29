f60cfdafe8eaac0004149aaaa1a948962fb53e10
Patrik Sandahl <kosmoskatten.software@gmail.com>
2017-10-29 21:06:22 +0100

    Skeleton.

---
+++ src/Scene/Camera.hs
@@ -0,0 +1,10 @@
+-- |
+-- Module: Scene.Camera
+-- Copyright: (c) 2017 Patrik Sandahl
+-- Licence: MIT
+-- Maintainer: Patrik Sandahl <patrik.sandahl@gmail.com>
+-- Stability: experimental
+-- Portability: portable
+module Scene.Camera
+    (
+    ) where
