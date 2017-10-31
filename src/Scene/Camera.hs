14f2e89c8d014431f9a08a4fa9384dfc35e87884
Patrik Sandahl <patrik.sandahl@ericsson.com>
2017-10-31 15:58:41 +0100

    Added some skeleton stuff.

--- src/Scene/Camera.hs
+++ src/Scene/Camera.hs
@@ -6,5 +6,38 @@
 -- Stability: experimental
 -- Portability: portable
 module Scene.Camera
-    (
+    ( Camera
+    , mkCamera
+    , matrix
     ) where
+
+import           Graphics.GL (GLfloat)
+import           Linear      (M44, V3, (*^))
+import           Scene.Math  (mkViewMatrix, up3d)
+
+data Direction = Direction
+    { vector :: !(V3 GLfloat)
+    } deriving Show
+
+data Camera = Camera
+    { moveDirection :: !Direction
+    , viewDirection :: !Direction
+    , position      :: !(V3 GLfloat)
+    } deriving Show
+
+mkCamera :: V3 GLfloat -> V3 GLfloat -> V3 GLfloat -> Camera
+mkCamera = undefined
+
+matrix :: Camera -> M44 GLfloat
+matrix camera =
+    mkViewMatrix (position camera)
+                 (viewspot (position camera) (viewDirection camera))
+                 up3d
+{-# INLINE matrix #-}
+
+viewspot :: V3 GLfloat -> Direction -> V3 GLfloat
+viewspot position' direction =
+    position' + (vista *^ vector direction)
+
+vista :: GLfloat
+vista = 10
