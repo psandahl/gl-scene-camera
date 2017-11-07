07987e3f989f1c32d42d8500ee9d39123d14b739
Patrik Sandahl <patrik.sandahl@ericsson.com>
2017-11-07 14:55:38 +0100

    Turning left. Lenses.

--- src/Scene/Camera.hs
+++ src/Scene/Camera.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TemplateHaskell #-}
 -- |
 -- Module: Scene.Camera
 -- Copyright: (c) 2017 Patrik Sandahl
@@ -12,20 +13,23 @@ module Scene.Camera
     , matrix
     , forward
     , backward
+    , turnLeft
     ) where
 
-import           Flow        ((<|))
-import           Graphics.GL (GLfloat)
-import           Linear      (M44, V3, (*^))
-import           Scene.Math  (Angle (..), fromEulerAngles, mkViewMatrix, up3d)
+import           Control.Lens (makeLenses, over, set, view, (%~), (.~), (^.))
+import           Flow         ((<|))
+import           Graphics.GL  (GLfloat)
+import           Linear       (M44, V3, (*^))
+import           Scene.Math   (Angle (..), addAngles, fromEulerAngles,
+                               mkViewMatrix, up3d)
 
 -- | Camera record. Opaque to the user.
 data Camera = Camera
-    { position      :: !(V3 GLfloat)
-    , viewDirection :: !Direction
-    , viewVector    :: !(V3 GLfloat)
-    , moveDirection :: !Direction
-    , moveVector    :: !(V3 GLfloat)
+    { _position      :: !(V3 GLfloat)
+    , _viewDirection :: !Direction
+    , _viewVector    :: !(V3 GLfloat)
+    , _moveDirection :: !Direction
+    , _moveVector    :: !(V3 GLfloat)
     } deriving Show
 
 -- | Direction record:
@@ -34,27 +38,30 @@ data Camera = Camera
 -- elevation is an 'Angle' with respect to the horizon. Zero angle is aligned
 -- with the horizon, a positive value points above and a negative below.
 data Direction = Direction
-    { heading   :: !(Angle GLfloat)
-    , elevation :: !(Angle GLfloat)
+    { _heading   :: !(Angle GLfloat)
+    , _elevation :: !(Angle GLfloat)
     } deriving Show
+
+makeLenses ''Camera
+makeLenses ''Direction
 
 -- | Make a camera from a 'V3' telling its initial position and two 'Direction'.
 -- The first is the view direction and the other is the move direction.
 mkCamera :: V3 GLfloat -> Direction -> Direction -> Camera
 mkCamera position' viewDirection' moveDirection' =
     Camera
-        { position = position'
-        , viewDirection = viewDirection'
-        , viewVector = fromDirection viewDirection'
-        , moveDirection = moveDirection'
-        , moveVector = fromDirection moveDirection'
+        { _position = position'
+        , _viewDirection = viewDirection'
+        , _viewVector = fromDirection viewDirection'
+        , _moveDirection = moveDirection'
+        , _moveVector = fromDirection moveDirection'
         }
 
 -- | From the 'Camera', produce a view matrix.
 matrix :: Camera -> M44 GLfloat
 matrix camera =
-    mkViewMatrix (position camera)
-                 (moveTo (position camera) (viewVector camera) vista)
+    mkViewMatrix (camera ^. position)
+                 (moveTo (camera ^. position) (camera ^. viewVector) vista)
                  up3d
 {-# INLINE matrix #-}
 
@@ -62,25 +69,34 @@ matrix camera =
 -- the camera's move direction.
 forward :: GLfloat -> Camera -> Camera
 forward distance camera =
-    camera
-        { position = moveTo (position camera) (moveVector camera) distance
-        }
+    set position
+        (moveTo (camera ^. position)
+                (camera ^. moveVector) distance)
+        camera
 {-# INLINE forward #-}
 
 -- | Move the 'Camera' backward by the specified distance using the negated
 -- move direction for the camera.
 backward :: GLfloat -> Camera -> Camera
 backward distance camera =
-    camera
-        { position = moveTo (position camera)
-                            (negate <| moveVector camera)
-                            distance
-        }
+    set position
+        (moveTo (camera ^. position)
+                (negate <| camera ^. moveVector) distance)
+        camera
 {-# INLINE backward #-}
+
+turnLeft :: Angle GLfloat -> Camera -> Camera
+turnLeft theta camera =
+    let viewDirection' = over heading (addAngles theta) <| view viewDirection camera
+        moveDirection' = over heading (addAngles theta) <| view moveDirection camera
+    in set viewDirection viewDirection' <|
+        set moveDirection moveDirection' <|
+        set viewVector (fromDirection viewDirection') <|
+        set moveVector (fromDirection moveDirection') camera
 
 fromDirection :: Direction -> V3 GLfloat
 fromDirection direction =
-    fromEulerAngles (heading direction) (elevation direction)
+    fromEulerAngles (direction ^. heading) (direction ^. elevation)
 
 moveTo :: V3 GLfloat -> V3 GLfloat -> GLfloat -> V3 GLfloat
 moveTo position' direction distance =
